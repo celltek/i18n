88d39828da479788aa2696c79f80556ad80bce45
celltek <technik@celltek.info>
2023-03-05 13:12:02 +0100

    Added Ext de/en

---
+++ ext/Extensions Module/english/extensions.php
@@ -0,0 +1,27 @@
+<?php
+define("_EXTENSIONS","Extensions");
+define("_EXTENSIONS_ASSISTANT","Using the module, you can install and update all extensions for your TekBASE.");
+
+define("_CE_OK_DATABASE","Database tables have been successfully created!");
+define("_CE_ERROR_CURL","Request not possible. PHP-cURL is not installed on the web server!");
+define("_CE_ERROR_ZIP","Request not possible. PHP-ZIP is not installed on the web server!");
+define("_CE_ERROR_SHOP","You have not purchased a license for the widget/module!");
+define("_CE_ERROR_DOMAIN","Domain does not match license!");
+define("_CE_ERROR_PAGE","This page does not exist.");
+
+define("_EXT_SIGIN","Sign In");
+define("_EXT_SIGOUT","Sign Out");
+define("_EXT_VERIFIED_DEV","Verified Developer");
+define("_EXT_COMPATIBILITY","Compatibility");
+define("_EXT_RENT_VERSION","Rent");
+define("_EXT_PAID_VERSION","Buy");
+define("_EXT_FREE_VERSION","Free");
+define("_EXT_TEST_VERSION","Test");
+define("_EXT_UNLOCK","Unlock");
+define("_EXT_BUYING","Buying");
+define("_EXT_RENTING","Renting");
+define("_EXT_INSTALLED","Installed");
+define("_EXT_REMOVE","Remove extension");
+define("_EXT_OPEN","Open");
+define("_EXT_INSTALL","Install");
+define("_EXT_UPDATE_AVAILABLE","Update available");
---
+++ ext/Extensions Module/german/extensions.php
@@ -0,0 +1,28 @@
+<?php
+define("_EXTENSIONS","Erweiterungen");
+define("_EXTENSIONS_ASSISTENT","Über das Module kannst du alle Erweiterungen für dein TekBASE installieren und updaten.");
+
+define("_CE_OK_DATABASE","Datenbank-Tabellen wurden erfolgreich erstellt!");
+define("_CE_ERROR_CURL","Abfrage nicht möglich. PHP-cURL ist nicht auf dem Webserver installiert!");
+define("_CE_ERROR_ZIP","Abfrage nicht möglich. PHP-ZIP ist nicht auf dem Webserver installiert!");
+define("_CE_ERROR_SHOP","Du hast keine Lizenz für das Widget / Module erworben!");
+define("_CE_ERROR_DOMAIN","Domain stimmt nicht mit Lizenz überein!");
+define("_CE_ERROR_PAGE","Diese Seite existiert nicht.");
+
+define("_EXT_SIGIN","Anmelden");
+define("_EXT_SIGOUT","Abmelden");
+define("_EXT_VERIFIED_DEV","Verifizierter Entwickler");
+define("_EXT_COMPATIBILITY","Kompatibilität");
+define("_EXT_RENT_VERSION","Mieten");
+define("_EXT_PAID_VERSION","Kaufen");
+define("_EXT_FREE_VERSION","Kostenlos");
+define("_EXT_TEST_VERSION","Testen");
+define("_EXT_TEST_VERSION","Testen");
+define("_EXT_UNLOCK","Freischalten");
+define("_EXT_BUYING","Kaufen");
+define("_EXT_RENTING","Mieten");
+define("_EXT_INSTALLED","Installiert");
+define("_EXT_REMOVE","Erweiterung entfernen");
+define("_EXT_OPEN","Öffnen");
+define("_EXT_INSTALL","Installieren");
+define("_EXT_UPDATE_AVAILABLE","Update verfügbar");
