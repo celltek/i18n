ac6be9de94d99b0a07010466b1feb81456242fd8
celltek <technik@celltek.info>
2023-04-15 17:09:47 +0200

    Admin English ts

--- TekBASE/admin/language/english/tserver.php
+++ TekBASE/admin/language/english/tserver.php
@@ -1,37 +1,37 @@
 <?php
 
-define("_TSERVERNOTALLFIELDS","Es wurden nicht alle mit einem * gekennzeichneten Felder ausgefüllt!");
-define("_TSERVERIPPORTEXIST","Es existiert schon ein Teamspeak Grundserver mit der IP und dem Port!");
-define("_TSERVERPATHEXIST","Das Verzeichnis existiert schon auf dem Rootserver!");
-define("_TSERVERDBUPERROR","Datenbankfehler! Der Teamspeak Grundeserver konnte nicht geändert werden!");
-define("_TSERVERDBUPDATED","Der Teamspeak Grundserver wurde geändert!");
-define("_TSERVERDBSAVEERROR","Datenbankfehler! Der Teamspeak Grundserver konnte nicht hinzugefügt werden!");
-define("_TSERVERCREATED","Der Teamspeak Grundserver wurde soeben hinzugefügt!");
-define("_TSERVERNEW","Teamspeak eintragen");
-define("_TSERVERNOENTRY","<b>Keine Teamspeak Grundserver vorhanden!</b>");
-define("_TSERVERDELETESEL","markierte Teamspeak Grundserver löschen");
-define("_TSERVERAPP","Programm*:");
+define("_TSERVERNOTALLFIELDS","Not all fields marked with * have been filled out!");
+define("_TSERVERIPPORTEXIST","There is already a Teamspeak base server with the IP and port!");
+define("_TSERVERPATHEXIST","The directory already exists on the root server!");
+define("_TSERVERDBUPERROR","Database error! The Teamspeak base server could not be changed!");
+define("_TSERVERDBUPDATED","The Teamspeak base server has been changed!");
+define("_TSERVERDBSAVEERROR","Database error! The Teamspeak base server could not be added!");
+define("_TSERVERCREATED","The Teamspeak base server has just been added!");
+define("_TSERVERNEW","Add Teamspeak");
+define("_TSERVERNOENTRY","<b>No Teamspeak base servers available!</b>");
+define("_TSERVERDELETESEL","delete selected Teamspeak base servers");
+define("_TSERVERAPP","Program*:");
 define("_TSERVERSERVER","Server IP*:");
-define("_TSERVERQUERYPORT","Queryport*:");
-define("_TSERVERPATH","Verzeichnis*:");
+define("_TSERVERQUERYPORT","Query port*:");
+define("_TSERVERPATH","Directory*:");
 define("_TSERVERPATHEXAMPLE","(ts2, ts3, ...)");
-define("_TSERVERADMIN","Serveradmin Login*:");
-define("_TSERVERADMINPASSWD","Serveradmin Passwort*:");
-define("_TSERVEREASYCONF","Zusätzliche Erweiterung:");
-define("_TSERVEREASYCONFOPEN","Easyconfig Editor öffnen");
+define("_TSERVERADMIN","Serveradmin login*:");
+define("_TSERVERADMINPASSWD","Serveradmin password*:");
+define("_TSERVEREASYCONF","Additional extension:");
+define("_TSERVEREASYCONFOPEN","Open Easyconfig Editor");
 define("_TSERVERTEAMSPEAK","Teamspeak");
-define("_TSERVERSTATUS","Der Teamspeakstatus wird nun neu gesetzt!");
-define("_TSERVERDBDELERROR","Datenbankfehler! Der Teamspeakserver konnte nicht gelöscht werden!");
-define("_TSERVERDBDELETED","Der Teamspeakserver wurde soeben aus dem Webinterface gelöscht!");
-define("_TSERVERRSERVER","<b>Rootserver:</b>");
+define("_TSERVERSTATUS","The Teamspeak status is now being set!");
+define("_TSERVERDBDELERROR","Database error! The Teamspeak server could not be deleted!");
+define("_TSERVERDBDELETED","The Teamspeak server has just been deleted from the web interface!");
+define("_TSERVERRSERVER","<b>Root server:</b>");
 define("_TSERVERCPUINFO","<b>CPU:</b>");
 define("_TSERVERRAM","<b>RAM:</b>");
 define("_TSERVERHDDISK","<b>HDD:</b>");
 define("_TSERVERTRAFFIC","<b>Traffic:</b>");
-define("_TSERVERTSDNS","TSDNS verfügbar:");
+define("_TSERVERTSDNS","TSDNS available:");
 
-define("_QUESTIONTSERVERCHANGE","Wollen sie den Status wirklich ändern?");
+define("_QUESTIONTSERVERCHANGE","Do you really want to change the status?");
 
-define("_ASSISTENTTSERVER","In diesem Modul können sie Teamspeak 2/3 Grundeserver einrichten und verwalten.");
+define("_ASSISTENTTSERVER","In this module you can set up and manage Teamspeak 2/3 base servers.");
 
 ?>
