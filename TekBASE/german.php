447bd4d1f7a99bb09a0e727623880613153fb9a8
celltek <technik@celltek.info>
2023-04-15 13:53:19 +0200

    TekBASE German

---
+++ TekBASE/german.php
@@ -0,0 +1,329 @@
+<?php
+
+////////////////////////////////////////////
+// ALLGEMEIN
+////////////////////////////////////////////
+define("_BACK","Zurück");
+define("_NEXT","Weiter");
+define("_SEND","Senden");
+define("_CALC","Berechnen");
+define("_DETAILS","Details");
+define("_CHECKOUT","Zur Kasse");
+define("_ADDTOBASKET","In den Warenkorb");
+define("_SENDORDER","Kostenpflichtig bestellen");
+define("_ERROR","Fehler");
+define("_CURRENCY","&euro;");
+define("_TAX","inkl. MwSt.");
+define("_PERCENT","%");
+define("_PLEASESELECT","Bitte auswählen");
+define("_PAGEGENERATION","Die Seite wurde aufgebaut in"); 
+define("_SECONDS","Sekunden");
+define("_CLOCK","Uhr");
+define("_LOGIN","Login");
+
+
+////////////////////////////////////////////
+// SHOP
+////////////////////////////////////////////
+define("_SHOPTOPPRODUCTS","TOP Produkte");
+define("_SHOPPRODUCTS","Produkte");
+define("_SHOPCODE","Artikel Nr.:");
+define("_SHOPPRICE","Grundpreis:");
+define("_SHOPALLPRICE","Gesamtpreis:");
+define("_SHOPONETIMEPRICE","Einmalige Gebühren:");
+define("_SHOPONETIMEINCLUDE","Einmalige Gebühren:");
+define("_SHOPDETAILS","Details");
+define("_SHOPINFO","Produktbeschreibung");
+define("_SHOPCALC","Einlösen");
+define("_SHOPADD","Produkt hinzufügen");
+define("_SHOPBASKET","Warenkorb");
+define("_SHOPPERSONAL","Persönliche Daten");
+define("_SHOPMEMBER","Kundendaten");
+define("_SHOPSHIPPING","Versandart");
+define("_SHOPPAYMENT","Zahlungsart");
+define("_SHOPCHECK","Bestellung prüfen");
+define("_SHOPYOURADDRESS","<b>Ihre Rechnungsadresse</b>");
+define("_SHOPYOURSHIPTO","<b>Ihre Lieferadresse</b>");
+define("_SHOPBASKETINFO","Hier finden Sie alle Artikel, die Sie während Ihres Besuches in den Warenkorb gelegt haben.<br><br>");
+define("_SHOPMEMBERINFO","Wenn Sie bereits Kunde sind können Sie hier Ihre Kennung sowie Ihr Passwort angeben und die 
+Bestellung weiterführen.<br><br>");
+define("_SHOPPERSONALINFO","Bitte füllen Sie alle Felder aus. Zu jeder Bestellung speichern wir mit absenden der Bestellung die IP Adresse und eventuelle Fake
+Bestellungen werden von uns zur Anzeige gebracht.<br><br>");
+define("_SHOPADDRESSINFO","Falls Sie eine abweichende Lieferanschrift wünschen so tragen Sie diese bitte hier ein.<br><br>");
+define("_SHOPSHIPPINGINFO","Sie können hier die Art des Versands festlegen. Bitte beachten Sie, dass die Versandkosten je nach
+Versandart variieren können.");
+define("_SHOPPAYMENTINFO","Wählen Sie bitte hier eine Zahlungsart aus.");
+define("_SHOPCHECKINFO","Kontrollieren Sie hier bitte Ihre Bestellung und Ihrer Adressangaben. Klicken Sie danach
+einfach auf [Kostenpflichtig bestellen] damit schließen Sie den Bestellvorgang ab.");
+define("_SHOPDISCOUNTTEXT","Sie haben einen Rabattcode? Geben Sie diesen hier ein:");
+define("_SHOPDISCOUNTUSETEXT","Verwendeter Rabattcode:");
+define("_SHOPDOMAIN","Domainprüfung");
+define("_SHOPISMYDOMAIN","Providerwechsel");
+define("_SHOPEMAILMYDOMAIN","Providerwechsel");
+define("_SHOPDOMAINNAME","<b>Domain:</b>");
+define("_SHOPTITLE","<b>Anrede:</b>");
+define("_SHOPMR","Herr");
+define("_SHOPMS","Frau");
+define("_SHOPFIRM","Firma");
+define("_SHOPFIRMNAME","Firmenname:");
+define("_SHOPSURNAME","<b>Vorname:</b>");
+define("_SHOPNAME","<b>Nachname:</b>");
+define("_SHOPBIRTHDAY","<b>Geburtsdatum:</b>");
+define("_SHOPBIRTHDAYEXAMPLE","(TT.MM.JJJJ)");
+define("_SHOPSTREET","<b>Strasse:</b>");
+define("_SHOPCITY","<b>Ort:</b>");
+define("_SHOPZIPCODE","<b>PLZ:</b>");
+define("_SHOPCOUNTRY","<b>Land:</b>");
+define("_SHOPEMAIL","<b>Email:</b>");
+define("_SHOPPHONE","<b>Telefon:</b>");
+define("_SHOPPHONEB","<b>Telefon:</b>");
+define("_SHOPTITLETWO","Anrede:");
+define("_SHOPSURNAMETWO","Vorname:");
+define("_SHOPNAMETWO","Nachname:");
+define("_SHOPSTREETTWO","Strasse:");
+define("_SHOPCITYTWO","Ort:");
+define("_SHOPZIPCODETWO","PLZ:");
+define("_SHOPCOUNTRYTWO","Land:");
+define("_SHOPCTITLE","Anrede:");
+define("_SHOPCFIRMNAME","Firmenname:");
+define("_SHOPCSURNAME","Vorname:");
+define("_SHOPCNAME","Nachname:");
+define("_SHOPCBIRTHDAY","Geburtsdatum:");
+define("_SHOPCSTREET","Strasse:");
+define("_SHOPCCITY","Ort:");
+define("_SHOPCZIPCODE","PLZ:");
+define("_SHOPCCOUNTRY","Land:");
+define("_SHOPCEMAIL","Email:");
+define("_SHOPCPHONE","Telefon:");
+define("_SHOPFIRMEMAIL","Email:");
+define("_SHOPFIRMPHONE","Telefon:");
+define("_SHOPFIRMFAX","Fax:");
+define("_SHOPTAXID","SteuerNr.:");
+define("_SHOPVATID","USt.-IdNr.:");
+define("_SHOPCUSTOMER","Inhaber:");
+define("_SHOPBANK","Bank:");
+define("_SHOPACCOUNT","Kontonummer:");
+define("_SHOPBANKCODE","Bankleitzahl:");
+define("_SHOPPASSWD","<b>Passwort:</b>");
+define("_SHOPMEMNAME","<b>Kundenkennung:</b>");
+define("_SHOPOR","oder");
+define("_SHOPIBAN","IBAN:");
+define("_SHOPBIC","BIC:");
+define("_SHOPERROR","Fehler");
+define("_SHOPBADMEMORPASS","Die Kundenkennung oder das Passwort ist falsch!");
+define("_SHOPGENERALTERMS","Ich habe die <a href=\"downloads/agb.pdf\" style=\"text-decoration:underline;\">AGB</a> gelesen und erkläre mit dem Absenden der Bestellung mein Einverständnis.");
+define("_SHOPACCEPTTERMS","Sie können die Bestellung nur absenden, wenn Sie den AGB zustimmen.");
+define("_SHOPCANCELLATIONPOLICY","Ich habe die <a href=\"downloads/widerrufsbelehrung.pdf\" style=\"text-decoration:underline;\">Widerrufsbelehrung</a> zur Kenntnis genommen. Ich stimme der Ausführung des Vertrages vor Ablauf der Widerrufsfrist ausdrücklich zu.");
+define("_SHOPACCEPTCANCELLATIONPOLICY","Sie können die Bestellung nur absenden, wenn Sie der Widerrufsbelehrung zustimmen.");
+define("_SHOPPRIVACYPOLICY","Hinweis: Alle Ihre Daten werden verschlüsselt übertragen. Ihr Angaben werden für die Erstellung eines Benutzerkontos und zur Vertragsausführung benötigt. Ein Verkauf oder Weitergabe Ihrer persönlichen Daten an Dritte erfolgt nicht. Sie haben jederzeit die Möglichkeit, Ihre hinterlegten Daten von uns löschen zu lassen.");
+define("_SHOPACCEPTPRIVACYPOLICY","Sie können die Bestellung nur absenden, wenn Sie uns die Verarbeitung Ihrer Daten erlauben.");
+define("_SHOPDATAPROCESSINGINFOS","Hinweis: Alle Ihre Daten werden verschlüsselt übertragen. Ihr Angaben werden für die Erstellung eines Benutzerkontos benötigt. Ein Verkauf oder Weitergabe Ihrer persönlichen Daten an Dritte erfolgt nicht. Sie haben jederzeit die Möglichkeit, Ihre hinterlegten Daten von uns löschen zu lassen. Haben Sie zur Zahlung einen Payment Anbieter gewählt,
+so werden die dafür Notwendigen Daten, wie zum Beispiel der Betrag und die Kundennummer übermittelt. Jeder Payment Anbieter stellt anderen Mindestanforderungen.");
+define("_SHOPNOCONSET","Der Datenverarbeitung wurde nicht zugestimmt! Sie können nur fortsetzen, wenn Sie uns die Verarbeitung Ihrer Daten erlauben.");
+
+define("_SHOPYOURORDER","Ihre Bestellung");
+define("_SHOPORDERNUMBER","Auftragsnummer:");
+define("_SHOPORDERDATE","Bestelldatum:");
+define("_SHOPTHXFORORDER","Wir bedanken uns für Ihre Bestellung und werden sie so schnell wie möglich ausführen:");
+define("_SHOPTERMSYES","Ich habe die AGB gelesen und akzeptiert: Ja");
+define("_SHOPTERMSNO","Ich habe die AGB gelesen und akzeptiert: Nein");
+define("_SHOPMYIP","<b>Meine IP:</b>");
+define("_SHOPMYTEXT","<b>Kommentar:</b>");
+define("_SHOPINFOTEXT","<b>Widerrufsrecht:</b>");
+define("_SHOPGENERALTERMSTEXT","<b>AGB:</b>");
+define("_SHOPNEWORDER","Eine neue Bestellung");
+define("_SHOPFINISH","Bestellung abgeschlossen");
+define("_SHOPEMAILSENDERROR","Die Bestellung konnte nicht gesendet werden!");
+define("_SHOPEMAILSENDOK","Die Bestellung wurde soeben an uns übermittelt!");
+define("_SHOPTODOTEXT","Kundenpaket, Rechnung und folgendes einrichten für");
+define("_SHOPEMAILTEXTCOLOR","#000000");
+define("_SHOPEMAILBACKGROUND","#F0F3F7");
+define("_SHOPEMAILTABLEHEADER","#4A79A9");
+define("_SHOPEMAILTABLEA","#D9E0E8");
+define("_SHOPEMAILTABLEB","#EAEEF2");
+define("_SHOPMEMBERIDENT","kd");
+define("_SHOPSTARTSITE","Zur Startseite");
+define("_SHOPURLBAD","Die angegebene URL ist nicht verfügbar. Bitte gehen Sie zurück und wählen Sie eine andere URL.");
+define("_SHOPURLOK","Die gewählte Domain ist verfügbar.");
+define("_SHOPTOBASKET","Zum Warenkorb");
+define("_SHOPPRODADDED","Der Artikel wurde soeben hinzugefügt!");
+define("_SHOPPRODADDERROR","Der Artikel konnte nicht hinzugefügt werden!");
+define("_SHOPEMPTYBASKET","Es befinden sich keine Artikel im Warenkorb!");
+define("_SHOPTABLEID","<b>#</b>");
+define("_SHOPTABLECODE","<b>Artikelnr.</b>");
+define("_SHOPTABLETEXT","<b>Beschreibung</b>");
+define("_SHOPTABLECOUNT","<b>Anzahl</b>");
+define("_SHOPTABLEONLY","<b>Stückpreis</b>");
+define("_SHOPTABLEALL","<b>Summe</b>");
+define("_SHOPTABLEDISCOUNT","<b>Rabatt:</b>");
+define("_SHOPTABLESUBTOTAL","<b>Zwischensumme:</b>");
+define("_SHOPTABLECREDITS","<b>Guthaben verrechnet:</b>");
+define("_SHOPTABLETAX","<b>Enthaltene {%tax%}% MwSt.:</b>");
+define("_SHOPTABLESHIPPING","<b>Versandkosten:</b>");
+define("_SHOPTABLEPAYMENT","<b>Zahlungsart:</b>");
+define("_SHOPTABLETOTAL","<b>Endbetrag:</b>");
+define("_SHOPSHIPPINGTYP","<b>Versandart:</b>");
+define("_SHOPPAYMENTTYP","<b>Zahlungsart:</b>");
+define("_SHOPSHIPTO","<b>Lieferadresse:</b>");
+define("_SHOPDELETEALL","Warenkorb leeren");
+define("_SHOPBASKETDELETED","Der Warenkorb wurde soeben geleert!");
+define("_SHOPDELETE","Artikel löschen");
+define("_SHOPPRODDELETED","Der Artikel wurde soeben gelöscht!");
+define("_SHOPEMAILEXIST","Diese Email existiert bereits! Bitte loggen Sie sich ein!");
+define("_SHOPADDRESSEXIST","Es existiert bereits ein Kunde mit diesen Daten! Bitte loggen Sie sich ein!");
+define("_SHOPBADEMAIL","Die angegebene Email Adresse ist ungültig!");
+define("_SHOPBADSUR","Der Vorname darf keine Sonderzeichen enthalten!");
+define("_SHOPSHORTSUR","Der angegebene Vorname ist zu kurz!");
+define("_SHOPBADLAST","Der Nachname darf keine Sonderzeichen enthalten!");
+define("_SHOPSHORTLAST","Der angegebene Nachname ist zu kurz!");
+define("_SHOPSHORTSTREET","Der Name der Strasse ist ungültig!");
+define("_SHOPSHORTCITY","Der Name der Stadt ist ungültig!");
+define("_SHOPSHORTZIP","Die Postleitzahl ist ungültig!");
+define("_SHOPSHORTBDAY","Das Geburtsdatum ist ungültig!");
+define("_SHOPSHORTPHONE","Die Telefonnummer ist ungültig!");
+define("_SHOPBADSHIPPING","Sie haben keine Versandart ausgewählt!");
+define("_SHOPBADCUSTOMER","Der Name des Inhabers darf keine Sonderzeichen enthalten!");
+define("_SHOPSHORTCUSTOMER","Der Name des Inhabers ist zu kurz!");
+define("_SHOPBADBANK","Der Name der Bank darf keine Sonderzeichen enthalten!");
+define("_SHOPSHORTBANK","Der Name der Bank ist zu kurz!");
+define("_SHOPBADBANKCODE","Die Bankleitzahl darf nur aus Zahlen bestehen!");
+define("_SHOPSHORTBANKCODE","Die Bankleitzahl ist zu kurz!");
+define("_SHOPBADACCOUNT","Die Kontonummer darf nur aus Zahlen bestehen!");
+define("_SHOPSHORTACCOUNT","Die Kontonummer ist zu kurz!");
+define("_SHOPBADIBAN","Die IBAN darf keine Sonderzeichen enthalten!");
+define("_SHOPSHORTIBAN","Die IBAN ist zu kurz!");
+define("_SHOPBADBIC","Die BIC darf keine Sonderzeichen enthalten!");
+define("_SHOPSHORTBIC","Die BIC ist zu kurz!");
+define("_SHOPBADPAYMENT","Sie haben keine Zahlungsart ausgewählt!");
+define("_SHOPCREDITSINFO","Die Bestellung soll mit meinem Guthaben ({%credits%}) verrechnet werden.<br><br><b>Hinweis:</b> Bitte wählen Sie bitte zusätzlich eine Zahlungsart, welche genutzt wird, wenn das Guthaben nicht ausreichen sollte.");
+define("_SHOPDBERROR","Es liegt ein Problem mit der Datenbank vor. Bitte wiederholen Sie den Schritt in einigen Minuten!");
+define("_SHOPPAYERROR","Die Bestellung kann leider nicht ausgeführt werden, da bei der Bezahlung ein Fehler aufgetreten ist.
+Bitte gehen Sie zurück und wählen Sie eine andere Zahlungsart oder versuchen Sie es erneut!");
+define("_SHOPAFFILIATE","Wurden Sie von einem Kunden geworben? Dann tragen Sie bitten seine Kundennummer hier ein:");
+define("_SHOPCREDITNOTE","Gutschrift");
+define("_STEPONE","Schritt 1");
+define("_STEPTWO","Schritt 2");
+define("_STEPTHREE","Schritt 3");
+define("_STEPFOUR","Schritt 4");
+define("_STEPADDRESS","Adressdaten");
+define("_STEPSHIPPING","Versandart");
+define("_STEPPAYMENT","Zahlungsart");
+define("_STEPSEND","Bestellung senden");
+define("_SHOPSEPA","Sepa Basis-Lastschrift-Mandat");
+define("_SHOPSEPASENDERROR","Das SEPA-Basis-Lastschrift-Mandat konnte nicht gesendet werden. Bitte kontaktieren Sie uns.");
+define("_SHOPSEPASENDED","Das SEPA-Basis-Lastschrift-Mandat wurde versendet.");
+define("_SHOPNOSIGNATURE","Die Unterschrift muss mit Ihrem Vor und Nachnamen übereinstimmen. Wenn der Kontoinhaber Max Mustermann heißt, muss die Unterschrift auch Max Mustermann lauten.");
+define("_SHOPSIGNATURE","Unterschrift (Zahlungspflichtiger):");
+
+
+////////////////////////////////////////////
+// NEU
+////////////////////////////////////////////
+define("_ACCESSDENIED","Zugriff verweigert!");
+define("_RESTRICTEDAREA","Zugang nicht gestattet!");
+define("_MEMBERSONLY","Dieser Bereich ist nur für registrierte Mitglieder!");
+define("_ADMINSONLY","Dieser Bereich ist nur für Admins!");
+define("_ONLYSUBSCRIBER","Dieser Bereich ist nur für registrierte Mitglieder!");
+define("_MODULENOTACTIVE","Das Modul ist zurzeit nicht aktiv!");
+define("_HOMEPROBLEM","Bitte im Adminbereich ein Modul für die Startseite festlegen!");
+define("_HOMEPROBLEMMEMBER","Leider kann diese Seite aus technischen Gründen nicht richtig angezeigt werden!");
+define("_ERROR","Fehler");
+
+
+////////////////////////////////////////////
+// CONTACT MODUL
+////////////////////////////////////////////
+define("_CONTACT","Kontakt");
+define("_CONTACTNAME","<b>Name:</b>");
+define("_CONTACTEMAIL","<b>eMail:</b>");
+define("_CONTACTSUBJECT","<b>Betreff:</b>");
+define("_CONTACTMSG","<b>Nachricht:</b>");
+define("_CONTACTSECURITY","<b>Nachrichten Code:</b>");
+define("_CONTACTNONAME","Sie haben keinen Namen angegeben!");
+define("_CONTACTNOEMAIL","Sie haben keine eMail angegeben!");
+define("_CONTACTNOSUBJECT","Sie haben keinen Betreff angegeben!");
+define("_CONTACTNOMSG","Sie haben keine Nachricht eingetragen!");
+define("_CONTACTNOCODE","Sie haben den Nachrichten Code nicht richtig angegeben!");
+define("_CONTACTDBUPERROR","Datenbankfehler! Eintrag konnte nicht gespeichert werden!");
+define("_CONTACTTHX","Danke für Ihre Nachricht, wir werden sie sobald als möglich
+beantworten. Sollten Sie aber nach 3 Tagen immer noch nichts
+von uns gehört haben, dann schicken Sie uns bitte eine Email!<br><br>Ihr Support Team");
+define("_CONTACTTEXT","Haben Sie Fragen zu unseren Angeboten, oder allgemeine Fragen? Dann schicken Sie uns einfach 
+über dieses Kontaktformular eine Nachricht. Wir werden diese dann umgehend beantworten.");
+define("_CONTACTNOCAPTCHA","Sie haben die Sicherheitsabfrage nicht bestätigt!");
+define("_CONTACTFALSECAPTCHA","Sie haben die Sicherheitsabfrage nicht bestanden!");
+define("_CONTACTDATAPROCESSINGINFOS","Hinweis: Alle Ihre Daten werden verschlüsselt übertragen. Ihre übermittelten Daten werden spätestens nach 60 Tagen automatisch gelöscht. Ein Verkauf oder Weitergabe Ihrer persönlichen Daten an Dritte erfolgt nicht. Sie haben jederzeit die Möglichkeit, Ihre hinterlegten Daten von uns löschen zu lassen.");
+define("_CONTACTNOCONSET","Der Datenverarbeitung wurde nicht zugestimmt! Sie können die Nachricht nur einsenden, wenn Sie uns die Verarbeitung Ihrer Daten erlauben. Bitte gehen Sie zurück.");
+
+
+////////////////////////////////////////////
+// IMPRESSUM MODUL
+////////////////////////////////////////////
+define("_IMPRESSUM","Impressum");
+define("_IMPRESSUMHOLDER","<b>Betreiber:</b>");
+define("_IMPRESSUMADDRESS","<b>Postanschrift:</b>");
+define("_IMPRESSUMTAXNUMBER","<b>Steuernummer:</b>");
+define("_IMPRESSUMVATNUMBER","<b>USt-ID-Nr:</b>");
+define("_IMPRESSUMVENUE","<b>Gerichtsstand:</b>");
+define("_IMPRESSUMREGISTER","<b>Registernummer:</b>");
+define("_IMPRESSUMCONTENTBY","<b>Inhaltlich Verantwortlicher:</b>");
+define("_IMPRESSUMWEBSITE","<b>Internet:</b>");
+define("_IMPRESSUMPHONEONE","<b>Telefon:</b>");
+define("_IMPRESSUMPHONETWO","<b>Hotline:</b>");
+define("_IMPRESSUMFAX","<b>Fax:</b>");
+define("_IMPRESSUMEMAIL","<b>Email:</b>");
+define("_IMPRESSUMDISCLAIMER","Haftungsausschluss");
+define("_IMPRESSUMCOPYRIGHT","Copyright");
+
+
+////////////////////////////////////////////
+// MONTHS
+////////////////////////////////////////////
+define("_JANUARY","Januar");
+define("_FEBRUARY","Februar");
+define("_MARCH","März");
+define("_APRIL","April");
+define("_MAY","Mai");
+define("_JUNE","Juni");
+define("_JULY","Juli");
+define("_AUGUST","August");
+define("_SEPTEMBER","September");
+define("_OCTOBER","Oktober");
+define("_NOVEMBER","November");
+define("_DECEMBER","Dezember");
+define("_JAN","Jan");
+define("_FEB","Feb");
+define("_MAR","Mar");
+define("_APR","Apr");
+define("_MAY","Mai");
+define("_JUN","Jun");
+define("_JUL","Jul");
+define("_AUG","Aug");
+define("_SEP","Sep");
+define("_OCT","Okt");
+define("_NOV","Nov");
+define("_DEC","Dez");
+
+
+////////////////////////////////////////////
+// DATA PRIVACY
+////////////////////////////////////////////
+define("_DATAPRIVACYPROCESSINGCONSET","Ich stimme der Datenverarbeitung zu.");
+
+define("_DATAPRIVACYACCEPTED","Data processing agreed");
+define("_DATAPRIVACYNAME","Name");
+define("_DATAPRIVACYADDRESS","Address");
+define("_DATAPRIVACYBIRTHDAY","Birthday");
+define("_DATAPRIVACYEMAIL","Email");
+define("_DATAPRIVACYIDCARD","ID Card");
+define("_DATAPRIVACYBANKACCOUNT","Bank details");
+define("_DATAPRIVACYID","ID:");
+define("_DATAPRIVACYCOLLECTEDDATA","Collected data:");
+define("_DATAPRIVACYSTATUS","Status:");
+
+define("_DATAPRIVACYUNLIMITEDDURATION","Storage of data until deletion request, in case of inactivity after 4 years.");
+define("_DATAPRIVACYPARTLY","Storage of data until deletion request. Data for verification purposes after 60 days.");
+define("_DATAPRIVACYCOMPLETEDELETION","Deletion of stored data after 60 days.");
+define("_DATAPRIVACYNOTSAVED","Data has been processed, no storage has taken place.");
+
+?>
---
+++ TekBASE/german_ratings.php
@@ -0,0 +1,64 @@
+<?php
+
+define("_RATINGS","Kundenbewertungen");
+define("_RATINGSNOVALUATIONS","Aktuell sind noch keine Bewertungen vorhanden!");
+define("_RATINGSVENDORSTATS","Shop-Statistik:");
+define("_RATINGSVALUATIONS","Bewertungen");
+define("_RATINGSCONCILIATIONSOK","erfolgreiche Schlichtungsverfahren");
+define("_RATINGSCONCILIATIONSNO","erfolglose Schlichtungsverfahren");
+define("_RATINGSCONCILIATIONOK","Das Schlichtungsverfahren war erfolgreich!");
+define("_RATINGSCONCILIATIONNO","Das Schlichtungsverfahren blieb erfolglos!");
+define("_RATINGSCUSTOMEROPINIONS","Kundenmeinungen");
+define("_RATINGSVALUATIONSFROMCTOC","Bewertungen von Kunden für Kunden");
+define("_RATINGSREPORTED","Diese Bewertung wird gerade geprüft um eine Lösung zu finden.");
+define("_RATINGSANSWERFROM","Antwort von");
+define("_RATINGSNEXTSITE","nächste Seite");
+define("_RATINGSPREVIOUSSITE","vorherige Seite");
+define("_RATINGSCURRENTVALUATION","Aktuelle Bewertung");
+define("_RATINGSADDRESS","Anschrift:");
+define("_RATINGSHOLDER","Berechtigter:");
+define("_RATINGSEMAIL","Email:");
+define("_RATINGSPHONE","Telefon:");
+define("_RATINGSFAX","Fax:");
+define("_RATINGSPOSITIVE","positiv");
+define("_RATINGSNEUTRAL","neutral");
+define("_RATINGSNEGATIVE","negativ");
+define("_RATINGSWEGARANTEE","Wir garantieren");
+define("_RATINGSGARANTEEINFO","Es werden von uns zu keiner Zeit Bewertungen gelöscht, ausser es handelt sich dabei nachweislich um unrechtmässige Bewertungen z.B.,
+wenn der Kunde seine Leistungen nicht bezahlt oder vorab von seinem Widerrufsrecht gebrauch gemacht hat.<br><br>
+Schlechte Bewertungen, welche im Schlichtungsverfahren zur vollen Zufriedenheit des Kunde geklärt werden konnten bleiben erhalten, werden jedoch nicht gewertet.
+<br><br>
+Wie das Bewertungssystem genau funktioniert und welche Sicherheiten es für Sie als Kunde bietet können Sie unter folgendem Link lesen.");
+define("_RATINGSNOTYET","Fehler - Keine Bewertung möglich");
+define("_RATINGSNOTYETINFO","Sie sind leider nicht berechtigt den Shopbetreiber zu bewerten. Falls Sie eine Email erhalten haben, mit der Bitte Ihren Anbieter zu
+bewerten, so rufen Sie den Link aus der Email noch einmal auf. Sollten Sie dann immer noch diesen Fehler hier sehen, senden Sie uns bitte Ihren Link an service@teklab.de.
+Wir werden dies dann umgehend kontrollieren.");
+define("_RATINGSBACK","Zurück");
+define("_RATINGSNORATING","Sie haben keine Bewertung vorgenommen. Bitte gehen Sie zurück und bewerten Sie den Shopbetreiber.");
+define("_RATINGSNOCOMMENT","Sie haben keinen Kommentar geschrieben. Bitte gehen Sie zurück und schreiben Sie einen Kommentar.");
+define("_RATINGSTOSHORT","Der Kommentar ist zu kurz. Bitte gehen Sie zurück und schreiben Sie einen ausführlicheren Kommentar.");
+define("_RATINGSALREADYRATED","Es wurde bereits eine Bewertung abgegeben. Sie können Ihre Bewertung nicht mehr ändern.");
+define("_RATINGSDBERROR","Ihre Bewertung konnte nicht gespeichert werden. Versuchen Sie es bitte erneut.");
+define("_RATINGSDBSAVED","Danke, dass Sie sich die Zeit genommen haben den Shopbetreiber zu bewerten. Sie können das Browserfenster jetzt schliessen");
+define("_RATINGSTOP","Top");
+define("_RATINGSFLOP","Flop");
+define("_RATINGSTEPONE","1. Schritt - Status");
+define("_RATINGSTEPTWO","2. Schritt - Bewerten");
+define("_RATINGSTEPTHREE","3. Schritt - Kommentieren");
+define("_RATINGSTEPFOUR","4. Schritt - Kontrollieren");
+define("_RATINGSTEPFIVE","5. Schritt - Abschicken");
+define("_RATINGSTEPONEINFOMEM","Sie sind als User {%customerid%} angemeldet. Wollen Sie die Bewertung für andere User anonym abgeben, so loggen Sie sich bitte zuerst aus. (<a href=\"members.php?op=Logout\" target=\"_blank\">ausloggen</a>)");
+define("_RATINGSTEPONEINFONOMEM","Sie sind derzeit nicht angemeldet oder haben noch keinen Kundenaccount. Daher wird die Bewertung anonym abgegeben. Wenn Sie jedoch Ihren Shopbetreiber unterstützen wollen, dann melden Sie sich bitte vorher im Kundenbereich an.");
+define("_RATINGSTEPTWOINFO","Vergeben Sie bitte Ihre Bewertung. Wählen Sie dazu das zutreffende Auswahlfelder aus.");
+define("_RATINGSTEPTHREEINFO","Vermeiden Sie bitte übermäßigen Gebrauch von Beleidigungen und Schimpfwörtern, da solche Bewertungen entfernt werden könnten. Bleiben Sie bitte immer sachlich und fair.");
+define("_RATINGSTEPFOURINFO","Bitte kontrollieren Sie noch einmal Ihre Bewertung, denn eine Bearbeitung ist später nicht mehr möglich. Haben Sie den Anbieter richtig bewertet? Sind Sie der Meinung, dass Ihr Kommentar so bleiben soll?");
+define("_RATINGSTEPFIVEINFO","Klicken Sie jetzt auf den Button \"Bewertung veröffentlichen\", wenn Sie die Bewertung so abgeben wollen.");
+define("_RATINGSSENDBUTTON","Bewertung veröffentlichen");
+define("_RATINGSNOTCONFIRMED","Fehler - Keine Bestätigung der Schlichtung möglich");
+define("_RATINGSNOTCONFIRMEDINFO","Sie sind leider nicht berechtigt die erfolgreiche Schlichtung zu bestätigen. Falls Sie eine Email erhalten haben, mit der Bitte, 
+eine Schlichtung des offenen Falls zu bestätigen,so rufen Sie den Link aus der Email noch einmal auf. Sollten Sie dann immer noch diesen Fehler hier sehen, setzen Sie sich mit Ihrem Shopbetreiber in Verbindung.");
+define("_RATINGSCONFIRMED","Schlichtung erfolgreich");
+define("_RATINGSCONFIRMEDINFO","Wir bedanken uns, dass Sie sich die Zeit genommen haben, die erfolgreiche Schlichtung des Falles zu bestätigen.");
+define("_RATINGSCOUNT","Stimmen");
+
+?>
