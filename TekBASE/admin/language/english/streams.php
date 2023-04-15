04171554f538899fa40289ce5a64cb37f9eb7946
celltek <technik@celltek.info>
2023-04-15 17:07:00 +0200

    Admin English streams

--- TekBASE/admin/language/english/streams.php
+++ TekBASE/admin/language/english/streams.php
@@ -1,48 +1,44 @@
 <?php
 
-define("_STREAMSNOTALLFIELDS","All required fields marked with an * weren't filled in!");
-define("_STREAMSIPPORTEXIST","A stream server with this ip and port already exists!");
-define("_STREAMSDBUPERROR","Database error! The stream server couldn't be changed!");
-define("_STREAMSDBUPDATED","The stream server was changed!");
-define("_STREAMSPATHEXIST","This directory already exists for this customer!");
-define("_STREAMSDBSAVEERROR","Database error! The stream server couldn't be added!");
-define("_STREAMSCREATED","The stream server was successfully added!");
-define("_STREAMSNEW","Add a stream server");
+define("_STREAMSNOTALLFIELDS","Not all fields marked with * have been filled out!");
+define("_STREAMSIPPORTEXIST","A stream server with the IP and port already exists!");
+define("_STREAMSDBUPERROR","Database error! The stream server could not be updated!");
+define("_STREAMSDBUPDATED","The stream server has been updated!");
+define("_STREAMSPATHEXIST","The directory already exists for this customer!");
+define("_STREAMSDBSAVEERROR","Database error! The stream server could not be saved!");
+define("_STREAMSINSTALLERROR","The stream server could not be installed!");
+define("_STREAMSCREATED","The stream server has just been created!");
+define("_STREAMSNEW","Create stream server");
 define("_STREAMSDELETESEL","delete selected stream servers");
 define("_STREAMSSTARTSEL","restart selected stream servers");
 define("_STREAMSSTOPSEL","stop selected stream servers");
 define("_STREAMSNOENTRY","<b>No stream servers available!</b>");
-define("_STREAMSDBDELERROR","Database error! The stream server couldn't be deleted!");
-define("_STREAMSDBDELETED","The stream server has been successfully deleted!");
-define("_STREAMSSTATUS","The selected stream server status will be changed!");
+define("_STREAMSDBDELERROR","Database error! The stream server could not be deleted!");
+define("_STREAMSDBDELETED","The stream server has just been deleted from the web interface!");
+define("_STREAMSSTATUS","The stream status is now being updated!");
 define("_STREAMSMEMBER","Customer*:");
-define("_STREAMSNAME","Name*:");
 define("_STREAMSAPP","Stream*:");
-define("_STREAMSBITRATE","Bitrate limitation:*");
-define("_STREAMSSERVER","Server IP*:");
+define("_STREAMSBITRATE","Bitrate limit:");
+define("_STREAMSSERVER","Server IP:");
 define("_STREAMSPORT","Port*:");
 define("_STREAMSSLOTS","Slots*:");
 define("_STREAMSPATH","Directory*:");
 define("_STREAMSPATHEXAMPLE","(sc, sc1, ...)");
-define("_STREAMSMEMADDRESS","<b>Address:</b>");
-define("_STREAMSMEMPHONE","Phone:");
-define("_STREAMSMEMEMAIL","Email:");
 define("_STREAMSRSERVER","<b>Root server:</b>");
 define("_STREAMSCPUINFO","<b>CPU:</b>");
 define("_STREAMSRAM","<b>RAM:</b>");
 define("_STREAMSHDDISK","<b>HDD:</b>");
 define("_STREAMSTRAFFIC","<b>Traffic:</b>");
-define("_STREAMSMULTIONE","32Bit");
-define("_STREAMSMULTITWO","64Bit");
-define("_STREAMSLOAD","Workload:");
-define("_STREAMSPERCENT","%");
-define("_STREAMSTRANSCODER","Transcoder allowed:");
-define("_STREAMSWEBFTP","WebFTP allowed:");
+define("_STREAMSTRANSCODER","Allow transcoder:");
+define("_STREAMSWEBFTP","Allow WebFTP:");
+define("_STREAMSSOFTLIMIT","Transcoder storage limit:");
+define("_STREAMSSOFTLIMITEXAMPLE","(100M, 1G, ...)");
+define("_STREAMSSOFTLIMITEXCEEDED","Limit exceeded on");
+define("_STREAMSLINKTOPRODUCT","Assign product:");
+define("_STREAMSEASYCONF","<b>Additional extension:</b>");
 
-define("_QUESTIONSTREAMSCHANGE","Are you sure you wish to make these changes?");
+define("_QUESTIONSTREAMSCHANGE","Do you really want to change the status?");
 
-define("_ASSISTENTSTREAMS","Here you can create new Shoutcast servers for you customers and configure settings as to whether the will have access to the Transcoder (Autopilot).
-MP3's can be played directly from the server using this transcoder when the DJ is offline. If the server appears with Red text in the list it has exceeded it's set 
-maximal Kbit rate. You can also start, stop or delete multiple server at the same time.");
+define("_ASSISTENTSTREAMS","In this module, you can create, start, stop, and delete new stream servers.");
 
 ?>
