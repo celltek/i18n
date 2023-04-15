e9fc46bdfcbff69fa221889358d525468fef38c3
celltek <technik@celltek.info>
2023-04-15 16:52:33 +0200

    Admin English Todo

--- TekBASE/admin/language/english/todo.php
+++ TekBASE/admin/language/english/todo.php
@@ -1,34 +1,34 @@
 <?php
-
-define("_TODONOTALLFIELDS","All required fields marked with an * weren't filled in!");
-define("_TODODBUPERROR","Database error! The task couldn't be changed!");
-define("_TODODBUPDATED","The task was changed!");
-define("_TODODBSAVEERROR","Database error! The task couldn't be added!");
-define("_TODODBCREATED","The task was successfully added!");
-define("_TODONEW","Add a task");
-define("_TODODELETESEL","delete selected tasks");
+	
+define("_TODONOTALLFIELDS","Not all fields marked with * have been filled!");
+define("_TODODBUPERROR","Database error! The task could not be updated!");
+define("_TODODBUPDATED","The task has been updated!");
+define("_TODODBSAVEERROR","Database error! The task could not be added!");
+define("_TODODBCREATED","The task has been added!");
+define("_TODONEW","Enter new task");
+define("_TODODELETESEL","Delete selected tasks");
 define("_TODONOENTRY","<b>No tasks available!</b>");
-define("_TODOSTATUS","The selected task will be changed!");
-define("_TODOOK","This task has been marked as completed!");
-define("_TODODBDELERROR","Database error! The task couldn't be deleted!");
-define("_TODODBDELETED","The task has been successfully deleted!");
+define("_TODOSTATUS","The task status is being reset!");
+define("_TODOOK","The task has been marked as completed!");
+define("_TODODBDELERROR","Database error! The task could not be deleted!");
+define("_TODODBDELETED","The task has been deleted from the web interface!");
 define("_TODODATE","Start date*:");
-define("_TODODATEEXAMPLE","(DD.MM.YYYY)");
-define("_TODODURATION","Due in (# of days)*:");
+define("_TODODURATION","Duration in days*:");
+define("_TODOTITLE","Title*:");
 define("_TODOTEXT","Text*:");
-define("_TODOURGENCY","Priority*:");
-define("_TODOWARNING","High");
+define("_TODOURGENCY","Urgency*:");
+define("_TODOWARNING","Urgent");
 define("_TODONORMAL","Normal");
 define("_TODOOPEN","Open");
-define("_TODOCLOSED","Finished");
-define("_TODOINFO","more info view icon");
+define("_TODOCLOSED","Completed");
+define("_TODOINFO","See icon for more information");
 define("_TODODAYS","Day(s)");
-define("_TODOEND","Due by");
+define("_TODOEND","Ends on");
 
-define("_QUESTIONTODOCHANGE","Are you sure you wish to make these changes?");
+define("_QUESTIONTODOCHANGE","Are you sure you want to change the status?");
 
-define("_ASSISTENTTODO","With this module you can create tasks for your admins. You can set the priority and due date. The tasks will be displays directly 
-in the admin interface. Your admins can then click task completed and the interface will save the admins name in the todo list and remove the task from 
-the interface window.");
+define("_ASSISTENTTODO","In this module, you can create tasks for your admins and specify whether they are urgent or not and how much time is allotted for them.
+The tasks will then be displayed directly on the homepage. If an admin clicks on a task as completed, the admin's name will be saved and the task will disappear
+from the homepage. Only the superadmin and the admin can create tasks.");
 
 ?>
