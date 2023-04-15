3ad65a1bcb72a353c8f9625a49dc8a3ca1ccdb37
celltek <technik@celltek.info>
2023-04-15 16:45:31 +0200

    Admin English emailacc

--- TekBASE/admin/language/english/emailacc.php
+++ TekBASE/admin/language/english/emailacc.php
@@ -1,46 +1,44 @@
 <?php
 
-define("_EMAILACCNOTALLFIELDS","Es wurden nicht alle mit einem * gekennzeichneten Felder ausgefüllt!");
-define("_EMAILACCDBUPERROR","Datenbankfehler! Das Email Konto konnte nicht geändert werden!");
-define("_EMAILACCDBUPDATED","Das Email Konto wurde geändert!");
-define("_EMAILACCDBSAVEERROR","Datenbankfehler! Das Email Konto konnte nicht hinzugefügt werden!");
-define("_EMAILACCDBCREATED","Das Email Konto wurde soeben hinzugefügt!");
-define("_EMAILACCNEW","Email eintragen");
-define("_EMAILACCDELETESEL","markierte Emails löschen");
-define("_EMAILACCNOENTRY","<b>Keine Emails vorhanden!</b>");
-define("_EMAILACCSTATUS","Die ausgewählten Email Konten werden nun gelöscht!");
-define("_EMAILACCDBDELERROR","Datenbankfehler! Das Email Konto konnte nicht gelöscht werden!");
-define("_EMAILACCDBDELETED","Das Email Konto wurde soeben aus dem Webinterface gelöscht!");
-define("_EMAILACCEXIST","Ein Email Konto mit dieser Email existiert schon!");
-define("_EMAILACCEMAIL","Email Adresse*:");
-define("_EMAILACCEMAILTWO","Email Adresse für Kopien:");
-define("_EMAILACCNAME","Absender*:");
-define("_EMAILACCTYPE","Email senden über:");
-define("_EMAILACCSHOST","SMTP Server:");
-define("_EMAILACCSPORT","SMTP Port:");
-define("_EMAILACCSUSER","Benutzername:");
-define("_EMAILACCSPASSWD","Passwort:");
-define("_EMAILACCSAUTH","Authentifikation:");
-define("_EMAILACCSECURE","Verschlüsselung:");
-define("_EMAILACCNOSECURE","keine");
-define("_EMAILACCSIGNATURE","Email Signatur:");
-define("_EMAILACCUSEDFOR","<b>Verwenden für</b>");
-define("_EMAILACCFORCMS","CMS Bereich:");
-define("_EMAILACCFORSHOP","Bestellungen:");
-define("_EMAILACCFORBILL","Rechnungen:");
-define("_EMAILACCFORSYSTEM","Allgemein:");
+define("_EMAILACCNOTALLFIELDS","Not all fields marked with * have been filled in!");
+define("_EMAILACCDBUPERROR","Database error! The email account could not be changed!");
+define("_EMAILACCDBUPDATED","The email account has been changed!");
+define("_EMAILACCDBSAVEERROR","Database error! The email account could not be added!");
+define("_EMAILACCDBCREATED","The email account has just been added!");
+define("_EMAILACCNEW","Enter email");
+define("_EMAILACCDELETESEL","delete marked emails");
+define("_EMAILACCNOENTRY","<b>No emails available!</b>");
+define("_EMAILACCSTATUS","The selected email accounts will now be deleted!");
+define("_EMAILACCDBDELERROR","Database error! The email account could not be deleted!");
+define("_EMAILACCDBDELETED","The email account has just been deleted from the web interface!");
+define("_EMAILACCEXIST","An email account with this email already exists!");
+define("_EMAILACCEMAIL","Email address*:");
+define("_EMAILACCEMAILTWO","Email address for copies:");
+define("_EMAILACCNAME","Sender*:");
+define("_EMAILACCTYPE","Send email via:");
+define("_EMAILACCSHOST","SMTP server:");
+define("_EMAILACCSPORT","SMTP port:");
+define("_EMAILACCSUSER","Username:");
+define("_EMAILACCSPASSWD","Password:");
+define("_EMAILACCSAUTH","Authentication:");
+define("_EMAILACCSECURE","Encryption:");
+define("_EMAILACCNOSECURE","none");
+define("_EMAILACCSIGNATURE","Email signature:");
+define("_EMAILACCUSEDFOR","<b>Use for</b>");
+define("_EMAILACCFORCMS","CMS area:");
+define("_EMAILACCFORSHOP","Orders:");
+define("_EMAILACCFORBILL","Invoices:");
+define("_EMAILACCFORSYSTEM","General:");
 define("_EMAILACCFORNEWSLETTER","Newsletter:");
-define("_EMAILACCPHPMAIL","PHP Mail");
+define("_EMAILACCPHPMAIL","PHP mail");
 define("_EMAILACCSMTP","SMTP");
-define("_EMAILACCBILLYES","Rechnungen");
+define("_EMAILACCBILLYES","Invoices");
 define("_EMAILACCCMSYES","CMS");
 define("_EMAILACCSHOPYES","Shop");
 define("_EMAILACCSYSTEMYES","System");
 define("_EMAILACCNEWSLETTERYES","Newsletter");
 
+define("_QUESTIONEMAILACCCHANGE","Do you really want to change the status?");
 
-define("_QUESTIONEMAILACCCHANGE","Wollen sie den Status wirklich ändern?");
-
-define("_ASSISTENTEMAILACC","In diesem Modul können sie verschiedene Email Konten für den Versand der Rechnungen, Bestellungen oder Systemmails anlegen.");
-
+define("_ASSISTENTEMAILACC","In this module, you can create various email accounts for sending invoices, orders, or system emails.");
 ?>
