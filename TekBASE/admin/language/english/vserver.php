0b30849ae0bb9e6e2412aa60e5fdb83685f2ad96
celltek <technik@celltek.info>
2023-04-15 17:07:27 +0200

    Admin English voiceserver

--- TekBASE/admin/language/english/vserver.php
+++ TekBASE/admin/language/english/vserver.php
@@ -1,43 +1,43 @@
 <?php
 
-define("_VSERVERNOTALLFIELDS","Es wurden nicht alle mit einem * gekennzeichneten Felder ausgefüllt!");
-define("_VSERVERIPPORTEXIST","Es existiert schon ein Voiceserver mit der IP und dem Port!");
-define("_VSERVERPATHEXIST","Das Verzeichnis existiert schon für diesen Kunden!");
-define("_VSERVERDBUPERROR","Datenbankfehler! Der Voiceserver konnte nicht geändert werden!");
-define("_VSERVERDBUPDATED","Der Voiceserver wurde geändert!");
-define("_VSERVERDBSAVEERROR","Datenbankfehler! Der Voiceserver konnte nicht hinzugefügt werden!");
-define("_VSERVERINSTALLERROR","Der Voiceserver konnte nicht installiert werden!");
-define("_VSERVERCREATED","Der Voiceserver wurde soeben hinzugefügt!");
-define("_VSERVERNEW","Voiceserver eintragen");
-define("_VSERVERNOENTRY","<b>Keine Voiceserver vorhanden!</b>");
-define("_VSERVERDELETESEL","markierte Voiceserver löschen");
-define("_VSERVERSTARTSEL","markierte Voiceserver neustarten");
-define("_VSERVERSTOPSEL","markierte Voiceserver stoppen");
-define("_VSERVERMEMBER","Kunde*:");
-define("_VSERVERAPP","Programm*:");
+define("_VSERVERNOTALLFIELDS","Not all fields marked with * have been filled out!");
+define("_VSERVERIPPORTEXIST","There is already a voice server with the IP and port!");
+define("_VSERVERPATHEXIST","The directory already exists for this customer!");
+define("_VSERVERDBUPERROR","Database error! The voice server could not be updated!");
+define("_VSERVERDBUPDATED","The voice server has been updated!");
+define("_VSERVERDBSAVEERROR","Database error! The voice server could not be added!");
+define("_VSERVERINSTALLERROR","The voice server could not be installed!");
+define("_VSERVERCREATED","The voice server has just been added!");
+define("_VSERVERNEW","Add voice server");
+define("_VSERVERNOENTRY","<b>No voice servers available!</b>");
+define("_VSERVERDELETESEL","delete selected voice servers");
+define("_VSERVERSTARTSEL","restart selected voice servers");
+define("_VSERVERSTOPSEL","stop selected voice servers");
+define("_VSERVERMEMBER","Customer*:");
+define("_VSERVERAPP","Program*:");
 define("_VSERVERSERVER","Server IP*:");
-define("_VSERVERPORT","Serverport*:");
-define("_VSERVERSLOTS","Slotanzahl*:");
-define("_VSERVERPATH","Verzeichnis*:");
-define("_VSERVERFILESHARING","Filesharing erlauben:");
+define("_VSERVERPORT","Server port*:");
+define("_VSERVERSLOTS","Number of slots*:");
+define("_VSERVERPATH","Directory*:");
+define("_VSERVERFILESHARING","Allow file sharing:");
 define("_VSERVERMUMBLE","Mumble");
 define("_VSERVERTEAMSPEAK","Teamspeak");
 define("_VSERVERVENTRILO","Ventrilo");
 define("_VSERVERVENTRILO214","Ventrilo 2.1.4");
 define("_VSERVERPATHEXAMPLE","(voice1, voice2, ...)");
-define("_VSERVERSTATUS","Der Voiceserverstatus wird nun neu gesetzt!");
-define("_VSERVERDBDELERROR","Datenbankfehler! Der Voiceserver konnte nicht gelöscht werden!");
-define("_VSERVERDBDELETED","Der Voiceserver wurde soeben aus dem Webinterface gelöscht!");
-define("_VSERVERWEBFTP","WebFTP erlauben:");
-define("_VSERVEREASYCONF","<b>Zusätzliche Erweiterung:</b>");
-define("_VSERVERLINKTOPRODUCT","Produkt zuweisen:");
+define("_VSERVERSTATUS","The voice server status is now being updated!");
+define("_VSERVERDBDELERROR","Database error! The voice server could not be deleted!");
+define("_VSERVERDBDELETED","The voice server has just been deleted from the web interface!");
+define("_VSERVERWEBFTP","Allow WebFTP:");
+define("_VSERVEREASYCONF","<b>Additional extension:</b>");
+define("_VSERVERLINKTOPRODUCT","Assign product:");
 define("_VSERVERTSDNS","<b>TSDNS:</b>");
-define("_VSERVERTSDNSDOMAIN","TSDNS Domain/Subdomain:");
-define("_VSERVERSTATUSPASSWD","Status: Passwortgeschützt");
-define("_VSERVERSTATUSNOPASSWD","Status: Nicht Passwortgeschützt");
+define("_VSERVERTSDNSDOMAIN","TSDNS domain/subdomain:");
+define("_VSERVERSTATUSPASSWD","Status: Password protected");
+define("_VSERVERSTATUSNOPASSWD","Status: Not password protected");
 
-define("_QUESTIONVSERVERCHANGE","Wollen sie den Status wirklich ändern?");
+define("_QUESTIONVSERVERCHANGE","Do you really want to change the status?");
 
-define("_ASSISTENTVSERVER","In diesem Modul können sie Voiceserver erstellen, löschen, neustarten oder stoppen.");
+define("_ASSISTENTVSERVER","In this module you can create, delete, restart or stop voice servers.");
 
 ?>
